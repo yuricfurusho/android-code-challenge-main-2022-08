899403672c3656718e1ef1fb826deafbd07242f4
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 11:42:51 -0600

    LEAG-0010: Adds dummy data to PostRepositoryImpl.kt

--- data-repo-impl/src/main/java/com/yuricfurusho/data_repo_impl/PostRepositoryImpl.kt
+++ data-repo-impl/src/main/java/com/yuricfurusho/data_repo_impl/PostRepositoryImpl.kt
@@ -1,4 +1,9 @@
 package com.yuricfurusho.data_repo_impl
 
-class PostRepositoryImpl {
+import com.yuricfurusho.domain_data.DummyData
+import com.yuricfurusho.domain_data.PostItem
+import com.yuricfurusho.domain_repo_interface.PostRepository
+
+class PostRepositoryImpl : PostRepository {
+    override fun getPostItemList(): List<PostItem> = DummyData.postItemList
 }
