049c46420ee5d766bbcd22f70a9c6fa1d70c162f
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 11:19:22 -0600

    LEAG-0008: Adds domain-usecase Layer and GetPostListUseCase.kt

--- composeUI/src/main/java/com/yuricfurusho/composeui/post/PostListViewModel.kt
+++ composeUI/src/main/java/com/yuricfurusho/composeui/post/PostListViewModel.kt
@@ -1,11 +1,12 @@
 package com.yuricfurusho.composeui.post
 
 import androidx.lifecycle.ViewModel
-import com.yuricfurusho.domain_data.DummyData
+import com.yuricfurusho.domain_usecase.GetPostListUseCase
 
+// TODO: we should have Hilt create the GetPostListUseCase instance for us
 class PostListViewModel : ViewModel() {
 
-    private val _postItemList = DummyData.postItemList
+    private val _postItemList = GetPostListUseCase().invoke()
     val postItemList = _postItemList
 
 }
