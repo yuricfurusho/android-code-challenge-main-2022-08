58e92c3d280327501475c0b2b69ac3fa4ba0116a
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 20:58:48 -0600

    LEAG-0017: Moves "PostRepositoryProvider" to "data-repository-impl" module from "kotlin_app" module

--- data-repository-impl/src/main/java/com/yuricfurusho/post/PostRepositoryProvider.kt
+++ data-repository-impl/src/main/java/com/yuricfurusho/post/PostRepositoryProvider.kt
@@ -1,14 +1,10 @@
-package life.league.challenge.kotlin.hilt
+package com.yuricfurusho.post
 
-import com.yuricfurusho.post.PostRepositoryImpl
-import com.yuricfurusho.post.PostRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
-//TODO Yuri: LEAG-0011: research about adequate place store hilt modules and their organization
-// right now because we are doing this here the app module requested domain-repo-interface as a dependency
 //TODO Yuri: LEAG-0012: research about correct scoping
 @Module
 @InstallIn(SingletonComponent::class)
