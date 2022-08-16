af8aebef6753d2d2d03692e997701b11f75201cb
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-16 15:51:10 -0600

    LEAG-0034: adds load "apiKey" function
    LEAG-0034: adds load "apiKey" function

--- data-localdatasource-interface/src/main/java/com/yuricfurusho/account/LoginLocalDataSource.kt
+++ data-localdatasource-interface/src/main/java/com/yuricfurusho/account/LoginLocalDataSource.kt
@@ -3,4 +3,5 @@ package com.yuricfurusho.account
 interface LoginLocalDataSource {
     fun getLoginAuth(username: String, password: String): String
     fun save(account: Account)
+    fun loadApiKey(apiKey: String): String
 }
