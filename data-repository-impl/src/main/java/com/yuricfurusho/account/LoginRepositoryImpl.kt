72736a0e7eef305b89c07a3c223c15ed13be9515
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 21:51:50 -0600

    LEAG-0022: Creates LoginUseCase and LoginRemoteDataSource.kt

--- data-repository-impl/src/main/java/com/yuricfurusho/account/LoginRepositoryImpl.kt
+++ data-repository-impl/src/main/java/com/yuricfurusho/account/LoginRepositoryImpl.kt
@@ -1,10 +1,16 @@
 package com.yuricfurusho.account
 
+import com.yuricfurusho.league.LoginRemoteDataSource
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
-    private val loginLocalDataSource: LoginLocalDataSource
+    private val loginLocalDataSource: LoginLocalDataSource,
+    private val loginRemoteDataSource: LoginRemoteDataSource
 ) : LoginRepository {
     override fun getLoginAuth(username: String, password: String): String =
         loginLocalDataSource.getLoginAuth(username, password)
+
+    override fun login(credentials: String): String =
+        loginRemoteDataSource.login(credentials)
+
 }
