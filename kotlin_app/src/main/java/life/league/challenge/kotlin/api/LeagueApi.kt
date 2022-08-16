887bd576cf4ba322d1d334e4953310844720f265
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 23:19:03 -0600

    LEAG-0015: Makes Hilt Implement LeagueApi

--- kotlin_app/src/main/java/life/league/challenge/kotlin/api/LeagueApi.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/api/LeagueApi.kt
@@ -8,15 +8,22 @@ import retrofit2.http.Header
  * Retrofit API interface definition using coroutines. Feel free to change this implementation to
  * suit your chosen architecture pattern and concurrency tools
  */
-interface Api {
+interface LeagueApi {
 
     @GET("login")
     suspend fun login(@Header("Authorization") credentials: String?): Account
 
+    companion object {
+        const val HOST = "https://engineering.league.dev/challenge/api/"
+    }
 }
 
 /**
  * Overloaded Login API extension function to handle authorization header encoding
  */
-suspend fun Api.login(username: String, password: String)
-        = login("Basic " + android.util.Base64.encodeToString("$username:$password".toByteArray(), android.util.Base64.NO_WRAP))
+suspend fun LeagueApi.login(username: String, password: String) = login(
+    "Basic " + android.util.Base64.encodeToString(
+        "$username:$password".toByteArray(),
+        android.util.Base64.NO_WRAP
+    )
+)
---
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/hilt/LeagueApiProvider.kt
@@ -0,0 +1,24 @@
+package life.league.challenge.kotlin.hilt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import life.league.challenge.kotlin.api.LeagueApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//TODO Yuri: LEAG-0011: research about adequate place store hilt modules and their organization
+// right now because we are doing this here the app module requested domain-repo-interface as a dependency
+//TODO Yuri: LEAG-0012: research about correct scoping
+@Module
+@InstallIn(SingletonComponent::class)
+class LeagueApiProvider {
+
+    @Provides
+    fun providesLeagueApi(): LeagueApi = Retrofit.Builder()
+        .baseUrl(LeagueApi.HOST)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(LeagueApi::class.java)
+}
