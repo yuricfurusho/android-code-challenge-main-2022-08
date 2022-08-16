09d50cc057be547f523571c69969d3682542d1aa
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-16 15:00:46 -0600

    LEAG-0032: prepares code to Save "apiKey" login response in SharedPreferences

--- data-repository-impl/src/main/java/com/yuricfurusho/account/LoginRepositoryImpl.kt
+++ data-repository-impl/src/main/java/com/yuricfurusho/account/LoginRepositoryImpl.kt
@@ -1,18 +1,31 @@
 package com.yuricfurusho.account
 
 import com.yuricfurusho.league.LoginRemoteDataSource
+import com.yuricfurusho.provider.DiskWriteScope
 import com.yuricfurusho.statewrapper.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
     private val loginLocalDataSource: LoginLocalDataSource,
-    private val loginRemoteDataSource: LoginRemoteDataSource
+    private val loginRemoteDataSource: LoginRemoteDataSource,
+    @DiskWriteScope private val writingCoroutineScope: CoroutineScope,
 ) : LoginRepository {
     override fun getLoginAuth(username: String, password: String): String =
         loginLocalDataSource.getLoginAuth(username, password)
 
     override suspend fun login(credentials: String): Flow<Result<Account>> =
-        loginRemoteDataSource.login(credentials)
-
+        loginRemoteDataSource.login(credentials).also { accountResultFlow ->
+            accountResultFlow.map { accountResult ->
+                accountResult.mapSuccessData { account ->
+                    writingCoroutineScope.launch(Dispatchers.Default) {
+                        loginLocalDataSource.save(account)
+                    }
+                }
+            }
+        }
 }
