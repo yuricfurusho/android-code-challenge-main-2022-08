e158d6ac770c95f02bc038ba9f83f4535d1bc2f5
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-16 12:00:31 -0600

    LEAG-0030: Adds CoroutineDispatcherProvider.kt

---
+++ data-remotedatasource-impl/src/main/java/com/yuricfurusho/account/CoroutineDispatcherProvider.kt
@@ -0,0 +1,17 @@
+package com.yuricfurusho.account
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+//TODO Yuri: LEAG-0012: research about correct scoping
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineDispatcherProvider {
+
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
