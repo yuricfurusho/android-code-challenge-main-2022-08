419767116b4fd5d1927bdd7269b065931bbd1817
Yuri Furusho <yuricfurusho@gmail.com>
2022-08-15 23:35:59 -0600

    LEAG-0024: Moves api call from MainActivity.kt to PostListActivity.kt

--- kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/main/MainActivity.kt
@@ -2,22 +2,11 @@ package life.league.challenge.kotlin.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.yuricfurusho.account.Account
-import com.yuricfurusho.account.LoginUseCase
 import com.yuricfurusho.post.PostListActivity
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import life.league.challenge.kotlin.R
-import javax.inject.Inject
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject lateinit var loginUseCase: LoginUseCase
 
     companion object {
         private const val TAG = "MainActivity"
@@ -30,22 +19,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent(this, PostListActivity::class.java))
         finish()
     }
-
-    override fun onResume() {
-        super.onResume()
-
-        // example api call to login, feel free to delete this and implement the call to login
-        // somewhere else differently depending on your chosen architecture
-        lifecycleScope.launch(Dispatchers.IO) {
-            try {
-                val account: Account = loginUseCase("hello", "world")
-                // TODO LEAG-0014: Replace Android logging with java.util.logging.Logger.
-                Log.v(TAG, account.apiKey ?: "")
-            } catch (t : Throwable) {
-                // TODO LEAG-0014: Replace Android logging with java.util.logging.Logger.
-                Log.e(TAG, t.message, t)
-            }
-        }
-    }
-
 }
--- ui-compose/src/main/java/com/yuricfurusho/post/PostListActivity.kt
+++ ui-compose/src/main/java/com/yuricfurusho/post/PostListActivity.kt
@@ -1,16 +1,24 @@
 package com.yuricfurusho.post
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.yuricfurusho.account.Account
+import com.yuricfurusho.account.LoginUseCase
 import com.yuricfurusho.composeui.ui.theme.AndroidCodeChallengeMainTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PostListActivity : ComponentActivity() {
 
     private val vm: PostListViewModel by viewModels()
+    @Inject lateinit var loginUseCase: LoginUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,26 @@ class PostListActivity : ComponentActivity() {
                 PostListScreen(vm)
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // example api call to login, feel free to delete this and implement the call to login
+        // somewhere else differently depending on your chosen architecture
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val account: Account = loginUseCase("hello", "world")
+                // TODO LEAG-0014: Replace Android logging with java.util.logging.Logger.
+                Log.v(TAG, account.apiKey ?: "")
+            } catch (t : Throwable) {
+                // TODO LEAG-0014: Replace Android logging with java.util.logging.Logger.
+                Log.e(TAG, t.message, t)
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "PostListActivity"
     }
 }
